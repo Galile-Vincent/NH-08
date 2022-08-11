86e3545352e0e149664df2f837540956c618250b
vincent <vincent.ua.248@gmail.com>
2022-08-11 17:16:29 +0800

    Initial Commit

---
+++ NH-08/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NH-08
+//
+//  Created by Vincent Chiang on 2022/8/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NH-08/NH_08App.swift
@@ -0,0 +1,17 @@
+//
+//  NH_08App.swift
+//  NH-08
+//
+//  Created by Vincent Chiang on 2022/8/11.
+//
+
+import SwiftUI
+
+@main
+struct NH_08App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
